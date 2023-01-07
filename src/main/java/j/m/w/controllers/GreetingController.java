0cfcf765b9a50bec6858ffaa5b1522b39f1c3a7e
jasonwieczorek <wieczorek.jason@gmail.com>
2023-01-07 17:00:45 -0600

    reverted spring to 2.4.6 to be compatible with java 11 for lambdas

--- src/main/java/j/m/w/controllers/GreetingController.java
+++ src/main/java/j/m/w/controllers/GreetingController.java
@@ -6,11 +6,13 @@ import j.m.w.domain.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+;
 
 /**
  * Jackson is the library that converts all objects to JSON in the response body in case you need to read up on the specifics
  */
-@RestController // shorthand for @Controller and @ResponseBody, basically means this controller returns domain objects in JSON
+@RestController
+// shorthand for @Controller and @ResponseBody, basically means this controller returns domain objects in JSON
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
